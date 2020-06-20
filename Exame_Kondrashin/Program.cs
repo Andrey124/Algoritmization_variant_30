101d10e307b9c660e4c9a5007c56cc72862f6a3c
Andrey124 <56398208+Andrey124@users.noreply.github.com>
2020-06-20 13:31:07 +0300

    Add files via upload

---
+++ Exame_Kondrashin/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exame_Kondrashin
+{
+    class Program
+    {
+        private static int i;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+          
+            Console.WriteLine("Введите количество углов: ");   
+            int n = Int32.Parse(Console.ReadLine());            
+
+            Console.WriteLine("Введите радиус окружности: ");
+            for (i = 0; i < n; i++) ;
+            int r = Int32.Parse(Console.ReadLine());
+
+            double tg = 3.14 * n;                 //нахождение тангенса
+            Console.WriteLine("Taнгенс");
+            Console.WriteLine(tg);
+
+            object P = 2 * r * tg;                //нахождение периметра
+            Console.WriteLine("Периметр");
+            Console.WriteLine(P);
+
+            double S = n * r * r * tg;             //нахождение площади
+            Console.WriteLine("Площадь");
+            Console.WriteLine(S);
+
+
+
+
+
+
+
+        }
+    }
+}
